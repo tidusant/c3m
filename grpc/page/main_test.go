aa50f5f89300f781571c3f2a87e60cf003318f42
duy ha <duyhph@gmail.com>
2021-01-17 08:18:29 +0700

    DEPLOY grpc/page change test

--- grpc/page/main_test.go
+++ grpc/page/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	pb "github.com/tidusant/c3m/grpc/protoc"
+	"github.com/tidusant/c3m/repo/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"fmt"
@@ -16,8 +18,8 @@ var testsession string = "random"
 var ctx context.Context
 var svc *service
 var appname = "test-grpc-page"
-var userId = ""
-var shopId = ""
+var userId = primitive.NilObjectID
+var shopId = primitive.NilObjectID
 var shopOriginId = "5955d130e761cf70ffb8e49b" //shopname: demo
 var m myRPC
 
@@ -30,8 +32,8 @@ func setup() {
 	//NOTE: must run auth_test before to have data in db
 	userLogin := m.Rpch.GetLogin(testsession)
 
-	userId = userLogin.UserId.Hex()
-	shopId = userLogin.ShopId.Hex()
+	userId = userLogin.UserId
+	shopId = userLogin.ShopId
 	//change to demoshop
 	shopOriginIdObj, _ := primitive.ObjectIDFromHex(shopOriginId)
 	shopchange := m.Rpch.UpdateShopLogin(testsession, shopOriginIdObj)
@@ -39,6 +41,19 @@ func setup() {
 		fmt.Println("Test fail: User can not change to origin shop in setup")
 		os.Exit(0)
 	}
+}
+func doCall(testname, action, params string, t *testing.T) models.RequestResult {
+	fmt.Println("\n\n==== " + testname + " ====")
+	resp, err := svc.Call(ctx, &pb.RPCRequest{AppName: appname, Action: action, Params: params, Session: testsession, UserID: userId.Hex(), ShopID: shopId.Hex(), UserIP: "127.0.0.1"})
+	if err != nil {
+		t.Fatalf("Test fail: Service error: %s", err.Error())
+	}
+	fmt.Printf("response return: %+v\n", resp)
+	//check test data
+	var rs models.RequestResult
+	json.Unmarshal([]byte(resp.Data), &rs)
+	fmt.Printf("Data return: %+v\n", rs)
+	return rs
 }
 func TestMain(m *testing.M) {
 	setup()
@@ -48,13 +63,18 @@ func TestMain(m *testing.M) {
 
 func TestUnknowAction(t *testing.T) {
 	fmt.Println("==== test TestUnknowAction ====")
-	rs, err := svc.Call(ctx, &pb.RPCRequest{AppName: appname, Action: "lasdf", Params: "abc,123", Session: testsession, UserID: userId, ShopID: shopId, UserIP: "127.0.0.1"})
+	rs, err := svc.Call(ctx, &pb.RPCRequest{AppName: appname, Action: "lasdf", Params: "abc,123", Session: testsession, UserID: userId.Hex(), ShopID: shopId.Hex(), UserIP: "127.0.0.1"})
 	if err != nil {
 		t.Fatalf("Test fail: Service error: %s", err.Error())
 	}
+	var result models.RequestResult
+	err = json.Unmarshal([]byte(rs.Data), &result)
+	if err != nil {
+		t.Fatalf("Test fail: %s", err.Error())
+	}
 	//check test data
 	fmt.Printf("Data return: %+v\n", rs)
-	if rs.Data != "Hello "+appname {
+	if result.Data != "Hello "+appname {
 		t.Fatalf("Test fail: not correct return string")
 	}
 
