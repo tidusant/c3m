d25c7d590738df736907a06fe5dca087ff4090e4
duy ha <duyhph@gmail.com>
2021-01-31 12:04:14 +0700

    DEPLOY lptest change build folder

--- lptest/serverhandle.go
+++ lptest/serverhandle.go
@@ -11,55 +11,24 @@ func SubmitTest(sex, tplname string, c *gin.Context) string {
 	content := c.PostForm("data")
 
 	//build content for test
-	buildFolder := templateFolder + "/" + tplname + "/build"
-	os.RemoveAll(buildFolder)
+	buildFolder := templateFolder + "/" + tplname
+	os.RemoveAll(buildFolder + "/build")
 	err := os.Mkdir(buildFolder, 0775)
 	if err != nil {
 		return err.Error()
 	}
-	//copy all css file
+	//copy tailwind css file
 	input, err := ioutil.ReadFile(schemeFolder + "/tailwind.css")
 	if err != nil {
 		return err.Error()
 	}
-	err = ioutil.WriteFile(buildFolder+"/tailwind.css", input, 0644)
+	err = ioutil.WriteFile(buildFolder+"/css/tailwind.css", input, 0644)
 	if err != nil {
 		return err.Error()
 	}
-	//loop css folder and copy
-	if _, err := os.Stat(buildFolder + "/../css"); !os.IsNotExist(err) {
-		items, _ := ioutil.ReadDir(buildFolder + "/../css")
-		for _, item := range items {
-			if !item.IsDir() {
-				input, err := ioutil.ReadFile(buildFolder + "/../css/" + item.Name())
-				if err != nil {
-					return err.Error()
-				}
-				err = ioutil.WriteFile(buildFolder+"/"+item.Name(), input, 0644)
-				if err != nil {
-					return err.Error()
-				}
-			}
-		}
-	}
-	//copy all js file
-	if _, err := os.Stat(buildFolder + "/../js"); !os.IsNotExist(err) {
-		items, _ := ioutil.ReadDir(buildFolder + "/../js")
-		for _, item := range items {
-			if !item.IsDir() {
-				input, err := ioutil.ReadFile(buildFolder + "/../js/" + item.Name())
-				if err != nil {
-					return err.Error()
-				}
-				err = ioutil.WriteFile(buildFolder+"/"+item.Name(), input, 0644)
-				if err != nil {
-					return err.Error()
-				}
-			}
-		}
-	}
+
 	//create content
-	err = ioutil.WriteFile(buildFolder+"/content.html", []byte(content), 0644)
+	err = ioutil.WriteFile(buildFolder+"/build/content.html", []byte(content), 0644)
 	if err != nil {
 		return err.Error()
 	}
